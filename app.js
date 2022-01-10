49cfeea65f0c491d574e77ad562f2cb73b93cdf2
md ashif reza <mdashifreza111@gmail.com>
2022-01-10 13:19:58 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+const path = require('path');
+var io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "./display.html"))
+})
+
+io.on('connection', (socket) => {
+
+    socket.on("join-message", (roomId) => {
+        socket.join(roomId);
+        console.log("User joined in a room : " + roomId);
+    })
+
+    socket.on("screen-data", function (data) {
+        data = JSON.parse(data);
+        var room = data.room;
+        var imgStr = data.image;
+        socket.broadcast.to(room).emit('screen-data', imgStr);
+    })
+})
+
+var server_port = process.env.PORT || 5000;
+http.listen(server_port, () => {
+    console.log("Started on : " + server_port);
+})
